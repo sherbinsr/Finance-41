6bc89d1bdb4bbd0e0fe15598cb269208c6b7e3ff
sherbinsr <sherbinsyles31@gmail.com>
2024-10-20 22:07:44 +0530

    user activity

--- Frontend/financial-advisor/src/App.js
+++ Frontend/financial-advisor/src/App.js
@@ -7,6 +7,7 @@ import Home  from './components/Home'
 import ContactUs  from './components/Contactus';
 import News from './components/News';
 import Topbar  from './components/Topbar';
+import UserAnalytics from './components/UserAnalytics';
 
 
 function App() {
@@ -15,12 +16,12 @@ function App() {
       <Topbar/>
       <Routes>      
       <Route path="/" element={<Home/>} />
-      <Route path="/auth" element={<Home/>} />
       <Route path="/chatbot" element={<Chatbot />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/home/:id" element={<Home />} />
       <Route path="/contactus" element={<ContactUs/>}/> 
+      <Route path="/users" element={<UserAnalytics/>}/> 
       <Route path="/news" element={<News/>}/> 
       </Routes>
     </Router>
