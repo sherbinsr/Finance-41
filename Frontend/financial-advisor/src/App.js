a3f10f6990f579ef15bfbe24b065becfd4fa9137
sherbinsr <sherbinsyles31@gmail.com>
2024-10-22 13:07:16 +0530

    created a react UI for markettrends

--- Frontend/financial-advisor/src/App.js
+++ Frontend/financial-advisor/src/App.js
@@ -8,7 +8,7 @@ import ContactUs  from './components/Contactus/Contactus';
 import News from './components/News/News';
 import Topbar  from './components/Topbar/Topbar';
 import NotFound from './components/NotFound/NotFound';
-
+import MarketTrends from './components/MarketTrends/MarketTrends'
 
 
 function App() {
@@ -23,6 +23,7 @@ function App() {
       <Route path="/home/:id" element={<Home />} />
       <Route path="/contactus" element={<ContactUs/>}/> 
       <Route path="/news" element={<News/>}/> 
+      <Route path="/trends" element={<MarketTrends/>}/> 
       <Route path="/404" element={<NotFound/>}/> 
       </Routes>
     </Router>
