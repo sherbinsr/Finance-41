a4da1298832fe2de6157acdb519ff3dc2bbc9507
sherbinsr <sherbinsyles31@gmail.com>
2024-10-23 12:02:17 +0530

    configured api call for educational resource

--- Frontend/financial-advisor/src/Service/NewsService.js
+++ Frontend/financial-advisor/src/Service/NewsService.js
@@ -7,8 +7,13 @@ const NewsService = {
     return response.data;
   },
   getLatestArticles: async () => {
-    const response = await axios.get('http://127.0.0.1:8000/getlatestarticles');  // Latest articles API
+    const response = await axios.get('http://127.0.0.1:8000/getlatestarticles');  
     return response.data;
+  },
+  getEducationalResources: async () => {
+    const response = await fetch('http://127.0.0.1:8000/educational_resources');
+    if (!response.ok) throw new Error('Failed to fetch educational resources');
+    return response.json();
   }
 };
 
