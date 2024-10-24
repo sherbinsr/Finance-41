eaa6e9d4a2989dea708c717bc1f46193b96dcdf1
sherbinsr <sherbinsyles31@gmail.com>
2024-10-24 20:57:11 +0530

    refactoring code for tq

--- Frontend/financial-advisor/src/Service/LoginService.js
+++ Frontend/financial-advisor/src/Service/LoginService.js
@@ -6,7 +6,7 @@ export const AuthService = {
   
   login: async (userData) => {
     try {
-      const response = await axios.post(`${API_URL}/login`, userData);
+      const response = await axios.post(`https://finance-41-1081098542602.us-central1.run.app/proxy/8000/login`, userData);
       return response.data;
     } catch (error) {
       throw error.response.data.detail || 'Login failed';
